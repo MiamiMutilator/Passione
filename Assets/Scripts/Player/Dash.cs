c8e72d4d4d485fad76e1e556f107d907818fe263
ChaosDais <73835757+ChaosDais@users.noreply.github.com>
2025-09-23 20:58:00 -0400

    Implement Basic Dash

---
+++ Assets/Scripts/Player/Dash.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Dash : IAction
+{
+    private readonly PlayerController player;
+    private readonly float dashVelocity;
+    private Vector3 lastMoveDirection;
+    private readonly CharacterController controller;
+
+    public Dash(PlayerController player, CharacterController controller, float dashVelocity)
+    {
+        this.player = player;
+        this.controller = controller;
+        this.dashVelocity = dashVelocity;
+
+        lastMoveDirection = Vector3.forward;
+    }
+
+    public void OnActivation()
+    {
+        lastMoveDirection = player.lastDirection;
+
+        Vector3 movement = lastMoveDirection * dashVelocity;
+        controller.Move(movement);
+
+        //Debug.Log("Dash vector: " + movement);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,26 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 500f;
-    [SerializeField] private Camera playerCamera;
+    public Camera playerCamera;
 
     private CharacterController _characterController;
     private Vector2 moveInput;
     private Vector3 moveDirection;
+    [HideInInspector] public Vector3 lastDirection;
+
+    [Header("Dash")]
+    [Range(0, 10)]
+    public float dashVelocity;
+
+    private IAction dash;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+        dash = new Dash(this, GetComponent<CharacterController>(), dashVelocity);
 
         if (!gameObject.CompareTag("Player")) Debug.LogWarning($"Give the Player tag to {gameObject.name}!");
     }
@@ -38,15 +47,23 @@ public class PlayerController : MonoBehaviour
     private void ApplyMovement()
     {
         _characterController.Move(moveSpeed * Time.deltaTime * moveDirection);
+        lastDirection = moveDirection;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        // Called when the Move input event triggers
+        // Tied to "Move" InputActionMap
         moveInput = context.ReadValue<Vector2>();
         moveDirection = new Vector3(moveInput.x, 0.0f, moveInput.y);
 
         //Debug.Log("Move called with value " + _input);
         //Debug.Log("Direction set to " + _direction);
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        // Tied to "Dash" InputActionMap
+        //print("Dash called with value " + context.action.triggered);
+        if (context.action.triggered) dash.OnActivation();
     }
 }
