ceaad461f47321ae2cd39240fab91c387ce6ee5f
alopez0213122 <alopez0213122@gmail.com>
2025-10-17 21:36:02 -0400

    New block system

--- Assets/Scripts/Enemy Scripts/Blocking.cs
+++ Assets/Scripts/Enemy Scripts/Blocking.cs
@@ -5,7 +5,9 @@ using UnityEngine.AI;
 public class Blocking : MonoBehaviour
 {
     public bool isBlockingHead;
+    public BoxCollider headBlock;
     public bool isBlockingBody;
+    public BoxCollider bodyBlock;
     public bool isInKOState;
     public int health = 5;
 
@@ -77,6 +79,15 @@ public class Blocking : MonoBehaviour
                 animator.SetBool("isWalking", true);
                 
             }
+            //else if (distance < distanceKeptAway) //for ranged enemy
+            //{
+            //    Vector3 directionAway = (transform.position - Player.position).normalized;
+
+            //    Vector3 retreatPosition = transform.position + directionAway * (fightingDistance - distance);
+
+            //    enemy.SetDestination(retreatPosition);
+            //    animator.SetBool("isWalking", true);
+            //}
             else 
             {
                 enemy.ResetPath();
@@ -93,6 +104,14 @@ public class Blocking : MonoBehaviour
                 }
             }
         }
+        Vector3 direction = Player.position - transform.position; //when enemy is blocking
+        direction.y = 0; 
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -101,14 +120,14 @@ public class Blocking : MonoBehaviour
 
         if (other.gameObject.CompareTag("BodyJab"))
         {
-            if (!isBlockingHead || (playerRage.enraged))
+            if (!bodyBlock.enabled || (playerRage.enraged))
             {
                 TakeDamage(1);
             }
         }
         else if (other.gameObject.CompareTag("HeadHook"))
         {
-            if (!isBlockingHead || (playerRage.enraged))
+            if (!headBlock.enabled || (playerRage.enraged))
             {
                 TakeDamage(3);
             }
@@ -148,18 +167,22 @@ public class Blocking : MonoBehaviour
             case 0: // Block body
                 actionTimer = Random.Range(2, 7);
                 isBlockingBody = true;
+                bodyBlock.enabled = true;
                 animator.SetBool("isBlockingBody", true);
                 yield return new WaitForSeconds(actionTimer);
                 isBlockingBody = false;
+                bodyBlock.enabled = false;
                 animator.SetBool("isBlockingBody", false);
                 break;
 
             case 1: // Block head
                 actionTimer = Random.Range(2, 7);
                 isBlockingHead = true;
+                headBlock.enabled = true;
                 animator.SetBool("isBlockingHead", true);
                 yield return new WaitForSeconds(actionTimer);
                 isBlockingHead = false;
+                headBlock.enabled = false;
                 animator.SetBool("isBlockingHead", false);
                 break;
         }
