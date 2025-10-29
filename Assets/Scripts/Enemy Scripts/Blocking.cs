dc8f7d9bb1d4487a7ed074cc225c51ea1055aee5
ChaosDais <73835757+ChaosDais@users.noreply.github.com>
2025-10-28 22:03:17 -0400

    Clean up Blocking.cs + fix EnemyPathing

--- Assets/Scripts/Enemy Scripts/Blocking.cs
+++ Assets/Scripts/Enemy Scripts/Blocking.cs
@@ -8,69 +8,45 @@ public class Blocking : MonoBehaviour
     public BoxCollider headBlock;
     public bool isBlockingBody;
     public BoxCollider bodyBlock;
-    //public bool isInKOState;
-    //public int health = 5;
 
     private int actionChosen;
     private int actionTimer;
     private bool isInAction;
-
-    //private bool recentlyHit = false;
-    //public float hitCooldown = 1f;
-
     private EnemyHealth healthComponent;
+    private EnemyPathing pathing;
     private Animator animator;
     private Coroutine currentAction;
 
-    //vfx
-    //public GameObject stunnedVFX;
-
-    //Enemy AI
-    public NavMeshAgent enemy;
-    public Transform Player;
-    public float distanceKeptAway = 2f;
-    public float fightingDistance = 3f; // kept at distanceKeptAway + 1
-    public float awarenessDistance = 5f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
         healthComponent = GetComponent<EnemyHealth>();
+        pathing = GetComponent<EnemyPathing>();
 
         headBlock.enabled = false;
         bodyBlock.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if health == 0, KO state bool is activated and KO coroutine is activated
-        //if (health <= 0 && !isInKOState)
-        //{
-        //    StartCoroutine(KOTimer());
-        //}
-
-        if (healthComponent.isInKOState)
-            SetBlockingAnimations(false);
+        if (healthComponent.isInKOState) SetBlockingAnimations(false);
         else
         {
             animator.SetBool("isBlockingBody", isBlockingBody);
             animator.SetBool("isBlockingHead", isBlockingHead);
         }
-        //animator.SetBool("isKO", isInKOState);
 
-        //pathing and AI (probably move later)
-        float distance = Vector3.Distance(Player.position, transform.position);
+        if (pathing == null) return;
+
         if (!healthComponent.isInKOState)
         {
-            if (distance > awarenessDistance)
+            if (pathing.state == EnemyPathingState.Idle)
             {
-                enemy.ResetPath();
                 animator.SetBool("isWalking", false);
             }
-            else if (distance > fightingDistance)
+            else if (pathing.state == EnemyPathingState.Chasing)
             {
+                //Debug.Log("Chasing");
                 if (isBlockingBody || isBlockingHead)
                 {
                     isBlockingBody = false;
@@ -78,22 +54,12 @@ public class Blocking : MonoBehaviour
                     isInAction = false;
                     SetBlockingAnimations(false);
                 }
-                enemy.SetDestination(Player.position);
                 animator.SetBool("isWalking", true);
                 
             }
-            else if (distance < distanceKeptAway) //for ranged enemy
-            {
-                Vector3 directionAway = (transform.position - Player.position).normalized;
-
-                Vector3 retreatPosition = transform.position + directionAway * (fightingDistance - distance);
-
-                enemy.SetDestination(retreatPosition);
-                animator.SetBool("isWalking", true);
-            }
             else 
             {
-                enemy.ResetPath();
+                //Debug.Log("In Attack Range");
                 animator.SetBool("isWalking", false);
                 if (!isInAction)
                 {
@@ -111,7 +77,8 @@ public class Blocking : MonoBehaviour
         {
             SetBlockingAnimations(false);
         }
-        Vector3 direction = Player.position - transform.position; //when enemy is blocking
+
+        Vector3 direction = pathing.player.position - transform.position; //when enemy is blocking
         direction.y = 0; 
 
         if (direction != Vector3.zero)
@@ -120,56 +87,12 @@ public class Blocking : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
         }
     }
-    /*
-    private void OnTriggerEnter(Collider other)
-    {
-        if (recentlyHit) return;
-        /*
-        if (other.gameObject.CompareTag("BodyJab"))
-        {
-            if (!bodyBlock.enabled || (playerRage.enraged))
-            {
-                TakeDamage(1);
-            }
-        }
-        else if (other.gameObject.CompareTag("HeadHook"))
-        {
-            if (!headBlock.enabled || (playerRage.enraged))
-            {
-                TakeDamage(3);
-            }
-        }
-        */
-        /*
-        if (other.gameObject.CompareTag("BodyJab") && isInKOState == true || other.gameObject.CompareTag("HeadHook") && isInKOState == true)
-        {
-            Destroy(gameObject);
-            //increased knockback punch done through the punch script if it collides with a KO'd enemy
-        }
-    }
-    /*
-    private IEnumerator KOTimer()
-    {
-        stunnedVFX.SetActive(true);
-        animator.SetBool("isKO", true);
-        isInKOState = true;
-        isBlockingBody = false;
-        isBlockingHead = false;
-        yield return new WaitForSeconds(5);
-        health = 5;
-        isInKOState = false;
-        animator.SetBool("isKO", false);
-        stunnedVFX.SetActive(false);
-    }
-    */
-
     private IEnumerator ActionTaken()
     {
         isBlockingHead = false;
         isBlockingBody = false;
         isInAction = false;
-        animator.SetBool("isBlockingBody", false);
-        animator.SetBool("isBlockingHead", false);
+        SetBlockingAnimations(false);
         isInAction = true;
         switch (actionChosen)
         {
@@ -211,19 +134,4 @@ public class Blocking : MonoBehaviour
         animator.SetBool("isBlockingHead", boolean);
         animator.SetBool("isBlockingBody", boolean);
     }
-    /*
-    private IEnumerator DamageCooldown()
-    {
-        recentlyHit = true;
-        yield return new WaitForSeconds(hitCooldown);
-        recentlyHit = false;
-    }
-    */
-    /*
-    private void TakeDamage(int amount)
-    {
-        health -= amount;
-        StartCoroutine(DamageCooldown());
-    }
-    */
 }
