03058669ba344ebab50828fa62e5d3247c88f4c5
alopez0213122 <alopez0213122@gmail.com>
2025-11-03 16:38:20 -0500

    push

---
+++ Assets/EnemyPunchHitbox.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyPunchHitbox : MonoBehaviour
+{
+    public BoxCollider hitbox;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.OnHit(3);
+                hitbox.enabled = false;
+            }
+
+        }
+
+        if (other.CompareTag("NearMiss"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player != null && player.IsDodging())
+            {
+                player.OnEvade();
+            }
+        }
+    }
+}
